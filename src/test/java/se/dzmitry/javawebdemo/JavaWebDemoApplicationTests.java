266800e2ffdaf054a96c644ffa5ad54726e32d14
dzmitrynikalayeu <89850579967n@gmail.com>
2025-04-25 13:56:53 +0200

    JavaWebDemo

---
+++ src/test/java/se/dzmitry/javawebdemo/JavaWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.dzmitry.javawebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
